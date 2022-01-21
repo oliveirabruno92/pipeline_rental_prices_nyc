487e03bce5a62056cfb5a8c3409abcd8b42f2521
Bruno Guimarães de Oliveira <bruno.guimaraes.oliveira@g.globo>
2022-01-21 01:06:39 -0300

    Add a removal for points outside of the boundary

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     logger.info("Converting last_review column to datetime..")
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    logger.info("Removing points outside of the boundaries..")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     logger.info("Saving cleaned data into a csv file..")
     df.to_csv("clean_sample.csv", index=False)
     
